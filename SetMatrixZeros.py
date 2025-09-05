a388f86eea436bc13656e5b8161e16064e69f4e8
bvedpathak <bhagyesh.vedpathak@gmail.com>
2025-09-05 17:53:29 -0500

    zeroing matrix

--- SetMatrixZeros.py
+++ SetMatrixZeros.py
@@ -64,4 +64,8 @@ matrix = [[0,1,2,0],
           [3,4,5,2],
           [1,3,1,5]]
 
+matrix = [[0,1,2,0],
+          [3,4,5,2],
+          [1,3,1,5]]
+
 print(f"The resultant matrix is: {Solution().setZeroes(matrix)}")
